c50304d4620d874ce5515aaedd9031b6224aea1f
shaselton-usds <shaselton.usds@gmail.com>
2021-11-29 11:11:13 -0800

    adding test coverage for table-of-contents. Oddly, the JSON schema matcher didn't like the prescription drug file having a  to a URL even though it wasn't included in the test suite

--- tests/spec/libs/in-network-rates/in-network-rates_spec.rb
+++ tests/spec/libs/in-network-rates/in-network-rates_spec.rb
@@ -1,11 +1,10 @@
 require "spec_helper"
 require "json"
-JsonMatchers.schema_root = "../schemas/in-network-rates"
 
 describe "in network schema" do
   it "has a valid json schema" do
     begin
-      expect{ match_response_schema("in-network-rates") }.not_to raise_error
+      expect{ match_response_schema("in-network-rates/in-network-rates") }.not_to raise_error
     rescue RSpec::Expectations::ExpectationNotMetError => e
       expect(e.message).not_to include "AError"
     end
@@ -14,12 +13,12 @@ describe "in network schema" do
   it "has valid JSON fee-for-service example" do
     json_data = JSON.parse(File.read("../examples/in-network-rates/in-network-rates-fee-for-service-sample.json"))
 
-    expect(json_data).to match_response_schema("in-network-rates")
+    expect(json_data).to match_response_schema("in-network-rates/in-network-rates")
   end
 
   it "has valid JSON bundle example" do
     json_data = JSON.parse(File.read("../examples/in-network-rates/in-network-rates-bundle-sample.json"))
 
-    expect(json_data).to match_response_schema("in-network-rates")
+    expect(json_data).to match_response_schema("in-network-rates/in-network-rates")
   end
 end
